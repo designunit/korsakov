37180849eb4cffd2c0e8937194ff6f7784181348
Roman Timashev <roman@tmshv.ru>
2021-09-02 18:40:50 +0300

    add initial PhaseSelect component

---
+++ src/components/PhaseSelect/index.tsx
@@ -0,0 +1,11 @@
+export type PhaseSelectProps = {
+
+}
+
+export const PhaseSelect: React.FC<PhaseSelectProps> = props => {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next"
 import { useEffect, useRef } from "react"
 import mapboxgl, { LngLatLike, maxParallelImageRequests } from "mapbox-gl"
+import { PhaseSelect } from "@/components/PhaseSelect"
 
 const key = 'pk.eyJ1IjoidG1zaHYiLCJhIjoiZjYzYmViZjllN2MxNGU1OTAxZThkMWM5MTRlZGM4YTYifQ.uvMlwjz7hyyY7c54Hs47SQ'
 
@@ -202,65 +203,9 @@ const Page: NextPage = () => {
                 position: 'absolute',
                 top: 0,
                 left: 0,
-                width: '100%',
-                height: '100%',
             }}>
-                <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-                    <h1 className="text-6xl font-bold">
-                        Welcome to{' '}
-                        <a className="text-blue-600" href="https://nextjs.org">
-                            Next.js!
-                        </a>
-                    </h1>
-
-                    <p className="mt-3 text-2xl">
-                        Get started by editing{' '}
-                        <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-                            pages/index.js
-                        </code>
-                    </p>
-
-                    <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-                        <a
-                            href="https://nextjs.org/docs"
-                            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                        >
-                            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-                            <p className="mt-4 text-xl">
-                                Find in-depth information about Next.js features and API.
-                            </p>
-                        </a>
-
-                        <a
-                            href="https://nextjs.org/learn"
-                            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                        >
-                            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-                            <p className="mt-4 text-xl">
-                                Learn about Next.js in an interactive course with quizzes!
-                            </p>
-                        </a>
-
-                        <a
-                            href="https://github.com/vercel/next.js/tree/master/examples"
-                            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                        >
-                            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-                            <p className="mt-4 text-xl">
-                                Discover and deploy boilerplate example Next.js projects.
-                            </p>
-                        </a>
-
-                        <a
-                            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                        >
-                            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-                            <p className="mt-4 text-xl">
-                                Instantly deploy your Next.js site to a public URL with Vercel.
-                            </p>
-                        </a>
-                    </div>
+                <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center bg-white">
+                    <PhaseSelect></PhaseSelect>
                 </main>
             </div>
         </>
