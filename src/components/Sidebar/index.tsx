0677b3703b8680b352ed213ed26b4e124b7c4748
Roman Timashev <roman@tmshv.ru>
2021-09-16 17:28:43 +0300

    sidebar toggle improvements

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -12,10 +12,15 @@ export const Sidebar: React.FC<SidebarProps> = props => {
     }, [props])
 
     return (
-        <aside className={`absolute top-0 left-0 m-3 sm:w-1/3 md:w-1/4 bg-gray-100 ease-in-out transition-all duration-300 z-10 ${props.open ? '' : 'transform -translate-x-full'}`}>
-            {props.children}
+        <aside className={`absolute top-0 left-0 sm:w-1/3 md:w-1/4 ease-in-out transition-all duration-300 z-10 ${props.open ? '' : 'transform -translate-x-full'}`}>
+            <div className="max-h-screen overflow-auto m-3 bg-gray-200">
+                {props.children}
+            </div>
 
-            <button onClick={onClick} className="absolute top-0 -right-0 bg-gray-100 px-2 py-2 transform translate-x-full">
+            <button
+                onClick={onClick}
+                className={`absolute top-3 ${props.open ? 'right-3' : '-right-3'} transition-all duration-300 bg-gray-100 px-2 py-2 transform translate-x-full`}
+            >
                 <ArrowRightIcon className={`w-4 h-4 transition-all duration-300 ${props.open ? 'transform rotate-180' : ''}`}></ArrowRightIcon>
             </button>
         </aside>
