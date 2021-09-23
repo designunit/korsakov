604adf97424b50b104a26b0bfdc152264ddc89a1
Roman Timashev <roman@tmshv.ru>
2021-09-23 17:24:33 +0300

    SwitchGroup component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,53 +4,14 @@ import { initMap, switchPhase, setLayerVisibility } from "@/map"
 import mapboxgl from "mapbox-gl"
 import { useRouter } from 'next/router'
 
-import { Menu, Switch, Transition } from '@headlessui/react'
+import { Menu, Transition } from '@headlessui/react'
 import { Layout } from "../Layout"
 import { Sidebar } from "../Sidebar"
 import { Collapse, CollapseItem, Radio } from "../Collapse"
 import { useTranslations } from "use-intl"
 import { LangButton } from "../LangButton"
 import { ImageDialog } from "../ImageDialog"
-
-type SwitchGroupProps = {
-    values: Array<{
-        label: string,
-        checked: boolean,
-    }>,
-    onChange?: (value: any, index: number) => void
-}
-
-const SwitchGroup: React.FC<SwitchGroupProps> = props => {
-    return (
-        <Switch.Group>
-            {props.values.map((item, i) => {
-                return (
-                    <div
-                        key={i}
-                        className="flex items-center px-4 py-2"
-                    >
-                        <Switch.Label className="mr-4 flex-1">{item.label}</Switch.Label>
-                        <Switch checked={item.checked}
-                            // onChange={(checked) => setEnabled(enableds.map((x, ii) => ii === i ? checked : x))}
-                            onChange={(checked) => {
-                                if (typeof props.onChange === 'function') {
-                                    props.onChange(checked, i)
-                                }
-                            }}
-                            className={`${item.checked ? 'bg-green-500' : 'bg-gray-500'
-                                } relative inline-flex items-center w-8 h-5 rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500`}
-                        >
-                            <span
-                                className={`${item.checked ? 'translate-x-4' : 'translate-x-1'
-                                    } inline-block w-3 h-3 transform bg-white rounded-full transition-transform`}
-                            />
-                        </Switch>
-                    </div>
-                )
-            })}
-        </Switch.Group>
-    )
-}
+import { SwitchGroup } from "../SwitchGroup"
 
 type LegendProps = {
     values: { label: string, color: string }[]
---
+++ src/components/SwitchGroup/index.tsx
@@ -0,0 +1,41 @@
+import { Switch } from '@headlessui/react'
+
+export type SwitchGroupProps = {
+    values: Array<{
+        label: string,
+        checked: boolean,
+    }>,
+    onChange?: (value: any, index: number) => void
+}
+
+export const SwitchGroup: React.FC<SwitchGroupProps> = props => {
+    return (
+        <Switch.Group>
+            {props.values.map((item, i) => {
+                return (
+                    <div
+                        key={i}
+                        className="flex items-center px-4 py-2"
+                    >
+                        <Switch.Label className="mr-4 flex-1">{item.label}</Switch.Label>
+                        <Switch checked={item.checked}
+                            // onChange={(checked) => setEnabled(enableds.map((x, ii) => ii === i ? checked : x))}
+                            onChange={(checked) => {
+                                if (typeof props.onChange === 'function') {
+                                    props.onChange(checked, i)
+                                }
+                            }}
+                            className={`${item.checked ? 'bg-green-500' : 'bg-gray-500'
+                                } relative inline-flex items-center w-8 h-5 rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500`}
+                        >
+                            <span
+                                className={`${item.checked ? 'translate-x-4' : 'translate-x-1'
+                                    } inline-block w-3 h-3 transform bg-white rounded-full transition-transform`}
+                            />
+                        </Switch>
+                    </div>
+                )
+            })}
+        </Switch.Group>
+    )
+}
