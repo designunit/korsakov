6415b6751128447431ed632051f634c7cc889864
Roman Timashev <roman@tmshv.ru>
2021-09-23 18:16:45 +0300

    add Mapbox component

---
+++ src/components/Mapbox/mapbox.ts
@@ -0,0 +1,21 @@
+import mapboxgl, { LngLatLike } from "mapbox-gl"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidG1zaHYiLCJhIjoiZjYzYmViZjllN2MxNGU1OTAxZThkMWM5MTRlZGM4YTYifQ.uvMlwjz7hyyY7c54Hs47SQ'
+
+export type MapboxOptions = {
+    center: LngLatLike
+    style: string
+    zoom: number
+    pitch: number
+    bearing: number
+    container: HTMLElement
+}
+
+export function createMap(options: MapboxOptions) {
+    var map = new mapboxgl.Map({
+        ...options,
+        antialias: true,
+    })
+
+    return map
+}
