1af42bda9b80618f8e33a22ce962a887e0606066
Roman Timashev <roman@tmshv.ru>
2022-09-17 21:14:02 +0300

    next config lint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     i18n: {
-        locales: ['ru', 'en'],
-        defaultLocale: 'ru',
+        locales: ["ru", "en"],
+        defaultLocale: "ru",
     },
     images: {
         minimumCacheTTL: 60,
